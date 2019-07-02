06916b23d90a4f99ea52b5a2d252d14bb791d08e
elenasbravo <52258981+elenasbravo@users.noreply.github.com>
2019-07-02 11:21:13 +0200

    Update deepvariant.sh

--- deepvariant.sh
+++ deepvariant.sh
@@ -1,1 +1,27 @@
+#!bin/bash
+  
+BIN_VERSION="0.8.0"
 
+REF_DIR=
+REF=
+
+INPUT_DIR=
+INPUT_BAM=
+
+OUTPUT_DIR=
+OUTPUT_VCF=
+mkdir -p "${OUTPUT_DIR}"
+
+N_SHARDS="8"
+
+sudo docker run\
+        -v "${INPUT_DIR}":"/indir"\
+        -v "${OUTPUT_DIR}:/outdir"\
+        -v "${REF_DIR}":"/refdir"\
+        gcr.io/deepvariant-docker/deepvariant:"${BIN_VERSION}"\
+        /opt/deepvariant/bin/run_deepvariant\
+        --model_type=WES\
+        --ref=/refdir/${REF}\
+        --reads=/indir/${INPUT_BAM}\
+        --output_vcf=/outdir/${OUTPUT_VCF}\
+        --num_shards=${N_SHARDS}
