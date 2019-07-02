63544d2a9f58a7994f6e0f25e6d39089a5916b7b
elenasbravo <52258981+elenasbravo@users.noreply.github.com>
2019-07-02 11:24:51 +0200

    Update strelka.sh

--- strelka.sh
+++ strelka.sh
@@ -1,1 +1,19 @@
+#!bin/bash
 
+REF_DIR=
+REF=
+
+INPUT_DIR=
+INPUT_BAM=
+
+WORKFLOW_DIR=
+
+#configuracion analisis
+configureStrelkaGermlineWorkflow.py\
+        --bam /${INPUT_DIR}/${INPUT_BAM}\
+        --referenceFasta /${REF_DIR}/${REF}\
+        --runDir ${WORKFLOW_DIR}\
+        --exome
+
+#ejecucion analisis
+${WORKFLOW_DIR}/runWorkflow.py -m local -j 8
