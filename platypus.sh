1eeb31d646c01b7674eef3dd912842ac96a33ba2
elenasbravo <52258981+elenasbravo@users.noreply.github.com>
2019-07-02 11:34:16 +0200

    Update platypus.sh

--- platypus.sh
+++ platypus.sh
@@ -8,3 +8,16 @@ INPUT_BAM=
 
 OUTPUT_DIR=
 OUTPUT_VCF=
+
+THREADS=
+#Platypus puede realizar la eliminación de duplicados, pero el resultado es el mismo que con GATK MarkDuplicates
+
+platypus callVariants\
+  --bamFiles /${INPUT_DIR}/${INPUT_BAM}\
+  --refFile /${REF_DIR}/${REF}\
+  -o /${OUTPUT_DIR}/{OUTPUT_VCF}\
+  --nCPU ${THREADS]\
+  --filterDuplicates=1
+
+bgzip ${OUTPUT_DIR}/${OUTPUT_VCF}
+tabix ${OUTPUT_DIR}/${OUTPUT_VCF}.gz
